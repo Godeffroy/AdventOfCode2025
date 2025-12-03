5e9414328694e456f4dd5a5ef609137a90cd9c29
Geoffroy Arenou <geoffroy.arenou@milton-innovation.com>
2025-12-03 13:41:01 +0100

    Day 02, part 1

--- AdventOfCode2025/main.cpp
+++ AdventOfCode2025/main.cpp
@@ -25,18 +25,106 @@ int main()
     println("| Advent of Code |");
     println("*----------------*");
 
-    Day_01_Part_2();
+    Day_02_Part_1();
 
     println("Result : " + to_string(result));
 	return 0;
 }
 
-// not 7475
+// result : 19128774598
+void Day_02_Part_1()
+{
+    ReadInputFile(2, 1);
+    string line = inputLines[0];
+    vector<long long> id1, id2;
+    inputLines.clear();
+    try
+    {
+        while (line.find(',') != -1)
+        {
+            inputLines.push_back(line.substr(0, line.find(',')));
+            id1.push_back(stoll(line.substr(0, line.find('-'))));
+            id2.push_back(stoll(line.substr(line.find('-') + 1)));
+            line = line.substr(line.find(',') + 1);
+        }
+        inputLines.push_back(line);
+        id1.push_back(stoll(line.substr(0, line.find('-'))));
+        id2.push_back(stoll(line.substr(line.find('-') + 1)));
+    }
+    catch (const std::exception& e)
+    {
+        println(e.what());
+    }
+
+    vector<long long> invalidID;
+    int cpt = 0;
+    for (string str : inputLines)
+    {
+        for (long long i = id1[cpt]; i <= id2[cpt]; i++)
+        {
+            string s = to_string(i);
+            if (to_string(i).length() % 2 != 0)
+                continue;
+            if (s.substr(0, to_string(i).length() / 2) == s.substr(to_string(i).length() / 2))
+                invalidID.push_back(i);
+        }
+        cpt++;
+    }
+    for (long long int id : invalidID)
+    {
+        result += id;
+    }
+}
+// result : 19128774598
+void Day_02_Part_1()
+{
+    ReadInputFile(2, 1);
+    string line = inputLines[0];
+    vector<long long> id1, id2;
+    inputLines.clear();
+    try
+    {
+        while (line.find(',')!=-1)
+        {
+            inputLines.push_back(line.substr(0, line.find(',')));
+            id1.push_back(stoll(line.substr(0, line.find('-'))));
+            id2.push_back(stoll(line.substr(line.find('-') + 1)));
+            line = line.substr(line.find(',') + 1);
+        }
+        inputLines.push_back(line);
+        id1.push_back(stoll(line.substr(0, line.find('-'))));
+        id2.push_back(stoll(line.substr(line.find('-') + 1)));
+    }
+    catch (const std::exception& e)
+    {
+		println(e.what());
+    }
+
+    vector<long long> invalidID;
+    int cpt = 0;
+    for (string str : inputLines)
+    {
+        for (long long i = id1[cpt]; i <= id2[cpt]; i++)
+        {
+            string s = to_string(i);
+            if (to_string(i).length() % 2 != 0)
+                continue;
+            if (s.substr(0, to_string(i).length() / 2) == s.substr(to_string(i).length() / 2))
+                invalidID.push_back(i);
+        }
+        cpt++;
+    }
+    for (long long int id : invalidID)
+    {
+        result += id;
+    }
+}
+
+// not 7475 - 6162
 void Day_01_Part_2()
 {
     ReadInputFile(1, 2);
     int vault = 50;
-    result = 0;
     int progress = 0;
     size_t total = inputLines.size();
     for (string line : inputLines)
@@ -55,7 +143,10 @@ void Day_01_Part_2()
         else
 			vault += 0;
 
-        while (vault >= 100)
+        if (vault == 0)
+            result++;
+
+        while (vault > 100)
         {
             vault -= 100;
             result++;
@@ -67,9 +158,6 @@ void Day_01_Part_2()
             result++;
         }
 
-        if (vault == 0)
-            result++;
-
         progress++;
     }
 }
@@ -78,7 +166,6 @@ void Day_01_Part_1()
 {
     ReadInputFile(1, 1);
     int vault = 50;
-    result = 0;
     int progress = 0;
     size_t total = inputLines.size();
     for (string line : inputLines)
--- AdventOfCode2025/main.h
+++ AdventOfCode2025/main.h
@@ -6,22 +6,27 @@
 #include <string>
 #include <vector>
 
-
+//https://cplusplus.com/reference/string/string/
 using std::string;
 using std::cout;
 using std::endl;
 using std::cin;
 using std::vector;
 using std::to_string;
+using std::stoi;
+using std::stol;
 
 #define print(x) cout<<x;
 #define println(x) cout << x << endl;
 
 
 vector<string> inputLines;
-int result;
+long long result = 0;
 
 void ReadInputFile(int day, int part);
 
 void Day_01_Part_1();
 void Day_01_Part_2();
+
+void Day_02_Part_1();
+void Day_02_Part_2();
